0180ff283cc90023b20cdaa444ae843562c9b266
João Lucas Pereira Rafael <joaosombrio@rede.ulbra.br>
2022-04-07 00:35:01 -0300

    Clone Nubank

--- DTM/aula05/nubank_clone/lib/main.dart
+++ DTM/aula05/nubank_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nubank_clone/pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Nubank Design Inspiration',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: 
+      home: HomePage(),
     );
   }
 }
